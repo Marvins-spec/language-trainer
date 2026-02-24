ec17bda733ab2bb181ae5bdccae6c490b6edff1b
Marvins-spec <littlebunny1900@gmail.com>
2026-02-24 12:26:45 +0700

    update_memory_engine.dart

--- lib/engine/memory_engine.dart
+++ lib/engine/memory_engine.dart
@@ -2,11 +2,63 @@ import 'dart:convert';
 import 'dart:math';
 import 'package:flutter/services.dart';
 import '../models/vocabulary.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class MemoryEngine {
 
   List<Vocabulary> words = [];
   int currentIndex = 0;
+
+  // FIX
+  Future<void> saveProgress() async {
+  final prefs = await SharedPreferences.getInstance();
+
+  Map<String, dynamic> progressMap = {};
+
+  for (var w in words) {
+    progressMap[w.word] = {
+      "correctCount": w.correctCount,
+      "wrongCount": w.wrongCount,
+      "memoryStrength": w.memoryStrength,
+      "isLearned": w.isLearned,
+      "state": w.state,
+      "nextReview": w.nextReview?.toIso8601String(),
+    };
+  }
+
+  await prefs.setString(
+      "progress", jsonEncode(progressMap));
+}
+
+  // load save
+  Future<void> loadProgress() async {
+  final prefs = await SharedPreferences.getInstance();
+
+  final savedData = prefs.getString("progress");
+
+  if (savedData == null) return;
+
+  final Map<String, dynamic> progressMap =
+      jsonDecode(savedData);
+
+  for (var w in words) {
+    if (progressMap.containsKey(w.word)) {
+      var data = progressMap[w.word];
+
+      w.correctCount = data["correctCount"];
+      w.wrongCount = data["wrongCount"];
+      w.memoryStrength = data["memoryStrength"];
+      w.isLearned = data["isLearned"];
+      w.state = data["state"];
+
+      if (data["nextReview"] != null) {
+        w.nextReview =
+            DateTime.parse(data["nextReview"]);
+      }
+    }
+  }
+}
+
 
   // ===== LOAD JSON =====
   Future<void> loadWords() async {
@@ -52,41 +104,35 @@ class MemoryEngine {
   }
 
   // ===== ANSWER =====
-  void answer(bool correct) {
-
+  void answer(bool correct) async {
+  
     var w = words[currentIndex];
-
+  
     if (correct) {
-
+  
       w.correctCount++;
       w.memoryStrength++;
-
+  
       w.nextReview = DateTime.now().add(
         Duration(days: w.memoryStrength * 2),
       );
-
+  
       if (w.memoryStrength >= 3) {
         w.state = "MASTERED";
         w.isLearned = true;
       } else {
         w.state = "LEARNING";
       }
-
+  
     } else {
-
+  
       w.wrongCount++;
       w.state = "REVIEW";
       w.nextReview = DateTime.now();
     }
-
+  
+    await saveProgress();   // 🔥 เพิ่มบรรทัดนี้
+  
     nextWord();
-  }
-
-  void nextWord() {
-
-    if (words.isEmpty) return;
-
-    currentIndex =
-        (currentIndex + 1) % words.length;
   }
 }
